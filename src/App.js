89e5beb9e526da67d8a3460bc12ec03d3de4340a
gurjeetsinghvirdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2021-12-03 16:43:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap');
+</style>
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <section className='hero-container'>
+      <div className='header'>
+        <h1><KeyboardBackspaceIcon className='icons' color='white' />ORDER INFORMATION</h1>
+      </div>
+      <div className='delievery'>
+        <h3>Delievery Address</h3>
+        <p>Please Choose Destination Address</p>
+      </div>
+    </section>
   )
 }
 
